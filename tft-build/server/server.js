005d29741fa3421f214a391213e62b5fddcfee55
Jonathan Oh <jonoh832@gmail.com>
2022-08-15 03:53:35 -0700

    changes

--- tft-build/server/server.js
+++ tft-build/server/server.js
@@ -9,33 +9,30 @@ const app = express();
 app.use(cors());
 const router = express.Router();
 
-// this is our MongoDB database
+
 const dbRoute =
   'mongodb://localhost:27017/tft';
 
-// connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 
-let db = mongoose.connection;
 
-db.once('open', () => console.log('connected to the database'));
 
-// checks if connection with the database is successful
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.once('open', () => console.log('connected to the database'));
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// this is our get method
-// this method fetches all available data in our database
+
 router.get('/getUnits', (req, res) => {
   Data.find((err, data) => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true, data: data });
   });
 });
+
+
 
 
 // append /api for our http requests
